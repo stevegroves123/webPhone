3f2e9022179c9fc9048ed913aed8099646911d1d
Steve Groves <stevegroves123@gmail.com>
2018-03-18 22:28:54 +0000

    Initial commit for very basic web browser (no status bar)

--- weatherPhone/ViewController.swift
+++ weatherPhone/ViewController.swift
@@ -7,19 +7,83 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate, WKNavigationDelegate {
+    
+    //MARK: Globals
+    let urlhttp:String = "http://"
+    
+    //MARK: Outlets
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    @IBOutlet weak var urLTextBox: UITextField!
+    @IBOutlet weak var homeButton: UIButton!
+    
+    //MARK: Actions
+    @IBAction func backButtonAction(_ sender: UIButton) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+    
+    @IBAction func forwardButtonAction(_ sender: UIButton) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    
+    @IBAction func homeButtonAction(_ sender: UIButton) {
+        let urlString:String = "www.apple.com/uk"
+        let url:URL = URL(string: urlhttp+urlString)!
+        let urlRequest:URLRequest = URLRequest(url: url)
+        webView.load(urlRequest)
+        
+        urLTextBox.text = urlString
+    }
+    
+    //MARK: Overrides
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        webView.navigationDelegate = self
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear( animated )
+        let urlString:String = "www.apple.com/uk"
+        let url:URL = URL(string: urlhttp+urlString)!
+        let urlRequest:URLRequest = URLRequest(url: url)
+        webView.load(urlRequest)
+        
+        urLTextBox.text = urlString
     }
 
-
+    //MARK: func's
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        var urlString:String = urLTextBox.text!
+        if (urLTextBox.text?.starts(with: "http://"))! || (urLTextBox.text?.starts(with: "https://"))!{
+            urlString = urLTextBox.text!
+        } else {
+            urlString = (urlhttp + urLTextBox.text!)
+        }
+        let url:URL = URL(string: urlString)!
+        let urlRequest:URLRequest = URLRequest(url: url)
+        webView.load(urlRequest)
+        
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        backButton.isEnabled = webView.canGoBack
+        forwardButton.isEnabled = webView.canGoForward
+        
+        urLTextBox.text = webView.url?.absoluteString
+    }
 }
-
