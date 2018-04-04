428de745b0747bebba1e2cb5a16b2f3f252d59cc
Steve Groves <stevegroves123@gmail.com>
2018-04-04 15:57:31 +0100

    WebBrowser
    Basic web browser using webKit.
    This has a back / home / forward button and a text box showing the full
    url

--- weatherPhone/ViewController.swift
+++ weatherPhone/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  weatherPhone
+//  WebPhone
 //
 //  Created by steve groves on 15/03/2018.
 //  Copyright © 2018 steve groves. All rights reserved.
@@ -12,7 +12,7 @@ import WebKit
 class ViewController: UIViewController, UITextFieldDelegate, WKNavigationDelegate {
     
     //MARK: Globals
-    let urlhttp:String = "http://"
+    let urlhttp:String = "https://"
     
     //MARK: Outlets
     @IBOutlet weak var webView: WKWebView!
@@ -35,8 +35,9 @@ class ViewController: UIViewController, UITextFieldDelegate, WKNavigationDelegat
     }
     
     @IBAction func homeButtonAction(_ sender: UIButton) {
-        let urlString:String = "www.apple.com/uk"
-        let url:URL = URL(string: urlhttp+urlString)!
+        let urlString:String = "www.westhampshireccg.nhs.uk/In-practice"
+        guard let url:URL = URL(string: urlhttp+urlString) else
+            { return }
         let urlRequest:URLRequest = URLRequest(url: url)
         webView.load(urlRequest)
         
@@ -49,14 +50,18 @@ class ViewController: UIViewController, UITextFieldDelegate, WKNavigationDelegat
         webView.navigationDelegate = self
     }
     
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
-
+ 
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear( animated )
-        let urlString:String = "www.apple.com/uk"
-        let url:URL = URL(string: urlhttp+urlString)!
+        let urlString:String = "www.westhampshireccg.nhs.uk/In-practice"
+        // guard is used to make sure the unwrapped url is real
+        guard let url:URL = URL(string: urlhttp+urlString) else
+            { return }
         let urlRequest:URLRequest = URLRequest(url: url)
         webView.load(urlRequest)
         
@@ -75,10 +80,11 @@ class ViewController: UIViewController, UITextFieldDelegate, WKNavigationDelegat
         let url:URL = URL(string: urlString)!
         let urlRequest:URLRequest = URLRequest(url: url)
         webView.load(urlRequest)
-        
+        // remove keyboard
         textField.resignFirstResponder()
         return true
     }
+    
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         backButton.isEnabled = webView.canGoBack
